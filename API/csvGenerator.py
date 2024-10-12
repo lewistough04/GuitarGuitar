c4c5ffa3b7672a0ae3e4c32d24774b464f4e78ff
Luke Latta <lukelatta03@gmail.com>
2024-10-12 15:41:56 +0100

    removed API folder

--- API/csvGenerator.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-import csv
-
-url = "https://www.guitarguitar.co.uk/hackathon/products"
-response = requests.get(url).text
-json_data = json.loads(response)
-
-print(json_data[2])
