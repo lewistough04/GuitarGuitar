62e1f98eb6afe5d33167294e3dab7fdd4fb94d80
Lewis Tough <2770287t@student.gla.ac.uk>
2024-10-13 05:17:54 +0100

    populate imports

--- findyoursound/populate.py
+++ findyoursound/populate.py
@@ -1,15 +1,14 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'findyoursound.settings')
-
+import requests
+import json
+import csv
 import django
 django.setup()
 from music.models import Gear, Artist, Genre
 
 def getJSON():
-    import requests
-    import json
-    import csv
 
     url = "https://www.guitarguitar.co.uk/hackathon/products"
     response = requests.get(url).text
