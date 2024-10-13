5669aa95d821b2d68c6330f16fec2e00115e2cde
Aiden Lindsay <aiden.o.lindsay@gmail.com>
2024-10-13 02:43:49 +0100

    some styling

--- frontend/src/sections/Return/ReturnComponent.js
+++ frontend/src/sections/Return/ReturnComponent.js
@@ -4,58 +4,6 @@ import { useLocation } from "react-router-dom";
 import Axios from "axios";
 
 function ReturnComponent(){
-
-    // const guitars = [
-    //     { "SKU_ID": "240409422030008", 
-    //         "ASN": "", 
-    //         "Category": "GUAG_1", 
-    //         "Online": true, 
-    //         "ItemName": "Metropolis Composer Element Electro Acoustic Guitar ", 
-    //         "Title": "", 
-    //         "BrandName": "Godin", 
-    //         "Description": null, 
-    //         "ProductDetail": `The Metropolis Composer LR Baggs Element, which is part of the Godin Acoustic lineup, is absolutely stunning and a real joy to play! It comes in a classic dreadnought size, perfect for that big and full sound.\nThe body is made of a Mahogany construction with a smooth semi-gloss finish—with a solid Mahogany top and back, which are complemented by layered Mahogany sides.\nThis guitar also has a Mahogany neck, so you can be sure to get rich and rounded tones, all the while being very well balanced with the addition of a Richlite fretboard, an Ebony bridge, and a 25.5” scale.\nAlso included is the LR Baggs system for convenient plug-and-play performance. Naturally, the Metropolis Composer LR Baggs Element is made right here in Canada!`,
-    //         "SalesPrice": 939.0, 
-    //         "PictureMain": "https://images.guitarguitar.co.uk/cdn/large/170/240409422030008f.jpg", 
-    //         "QtyInStock": 1, 
-    //         "QtyOnOrder": 10, 
-    //         "Colour": 7, 
-    //         "Pickup": 1, 
-    //         "BodyShape": 10, 
-    //         "CreatedOn": "2024-04-19T11:24:06.757", 
-    //         "ImageUrls": null, 
-    //         "Rating": 10.0, 
-    //         "GlasgowQty": 1, 
-    //         "EdinburghQty": 1, 
-    //         "NewcastleQty": 0 
-    //     }, 
-    //     { "SKU_ID": "240409422030008", 
-    //         "ASN": "", 
-    //         "Category": "GUAG_1", 
-    //         "Online": true, 
-    //         "ItemName": "Metropolis Composer Element Electro Acoustic Guitar ", 
-    //         "Title": "", 
-    //         "BrandName": "Godin", 
-    //         "Description": null, 
-    //         "ProductDetail": "Product 2",
-    //         "SalesPrice": 939.0, 
-    //         "PictureMain": "https://images.guitarguitar.co.uk/cdn/large/170/240409422030008f.jpg", 
-    //         "QtyInStock": 1, 
-    //         "QtyOnOrder": 10, 
-    //         "Colour": 7, 
-    //         "Pickup": 1, 
-    //         "BodyShape": 10, 
-    //         "CreatedOn": "2024-04-19T11:24:06.757", 
-    //         "ImageUrls": null, 
-    //         "Rating": 10.0, 
-    //         "GlasgowQty": 1, 
-    //         "EdinburghQty": 1, 
-    //         "NewcastleQty": 0 
-    //     }
-    // ];
-
-    // assume i am getting from api and returns in format 
-    // array of dictionaries
     const location = useLocation();
     const [guitars, setGuitars] = useState([]);
     const [guitarIndex, setGuitarIndex] = useState(0);
@@ -63,9 +11,9 @@ function ReturnComponent(){
     useEffect(() => {
         const fetchGuitars = async () => {
             try {
-                const artistNames = location.state?.artists || []; // Assuming artists are passed from previous component
+                const artistNames = location.state?.artists || []; 
                 const response = await Axios.post("http://localhost:8000/api/gear/", { artists: artistNames });
-                setGuitars(response.data); // Set fetched guitars to state
+                setGuitars(response.data);
             } catch (error) {
                 console.error("Error fetching guitar data:", error);
             }
@@ -84,17 +32,9 @@ function ReturnComponent(){
     }
 
     const currentGuitar = guitars[guitarIndex];
-    //const fetchGuitars = async () => {
-      //  const res = await fetch(url);
-        //const d = await res.json();
-        //return setGuitar(d);
-    //}
-    
-    //useEffect(() => {
-      //  fetchGuitars();
-    //}, []);
 
     return(
+        <>
         <div className="return-div">
             <div>
                 <img
@@ -102,7 +42,6 @@ function ReturnComponent(){
                     className="guitar-image"
                     alt={currentGuitar.item_name}
                 />
-                <button className="next-button" onClick={nextGuitar}>Next</button>
             </div>
 
             <div className="guitar-text">
@@ -118,6 +57,8 @@ function ReturnComponent(){
                 <p className="guitar-description">{currentGuitar.productDetail}</p>
             </div>
         </div>
+        <button className="next-button" onClick={nextGuitar}>Next</button>
+        </>
     );
 }
 
